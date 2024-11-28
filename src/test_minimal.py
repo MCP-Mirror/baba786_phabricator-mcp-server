d853b3bd79f0e5b84b38ef2889473830fa99ca64
Sudhanshu <sudhanshu9252@gmail.com>
2024-11-28 10:50:49 +0530

    Add minimal test setup with .env support

---
+++ src/test_minimal.py
@@ -0,0 +1,24 @@
+import os
+from phabricator import Phabricator
+
+def test_connection():
+    phab = Phabricator(
+        host='https://phabricator.wikimedia.org/api/',
+        token=os.getenv('PHABRICATOR_TOKEN')
+    )
+    phab.update_interfaces()
+    
+    # Test with a simple whoami call
+    print('Connected as:', phab.user.whoami()['userName'])
+    
+    # Try getting a task
+    task_id = input('Enter task ID (without T): ')
+    task = phab.maniphest.search(constraints={'ids': [int(task_id)]}).data
+    if task:
+        print(f"\nFound task T{task[0]['id']}: {task[0]['fields']['name']}")
+
+if __name__ == '__main__':
+    if not os.getenv('PHABRICATOR_TOKEN'):
+        print('Error: PHABRICATOR_TOKEN not set')
+        exit(1)
+    test_connection()
